e57892897fd696c68f7138456a07fa3abe67d7e9
Nadie Ivanitska <103319310+NadieIvi@users.noreply.github.com>
2022-10-22 14:38:14 +0300

    Added links to GitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
 import dictionary from "./dictionary.png";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={dictionary} className="dictionary-icon" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header mt-5">
+          <img src={dictionary} className="dictionary-icon" alt="logo" />
+        </header>
 
-      <footer>
-        This project was coded by Nadie Ivanitska and is{" "}
-        <a
-          href="https://github.com/NadieIvi/react-weather-final"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-soursed on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://lovely-taffy-3e2a51.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          hosted on Netlify
-        </a>
-        <a
-          href="https://www.flaticon.com/free-icons/dictionary"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dictionary icons created by Freepik - Flaticon
-        </a>
-      </footer>
+        <footer>
+          This project was coded by Nadie Ivanitska and is{" "}
+          <a
+            href="https://github.com/NadieIvi/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-soursed on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://6353d35b3255bf064b840b27--moonlit-mousse-9a24f0.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+          <br />
+          <a
+            href="https://www.flaticon.com/free-icons/dictionary"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="fs-6"
+          >
+            Dictionary icons created by Freepik - Flaticon
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
